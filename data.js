4ac07af173d7a99451d0116e5171531bf40570ca
leonardodastin <leonardodastin@gmail.com>
2025-12-01 22:17:26 +0530

    Dec1

--- data.js
+++ data.js
@@ -16,52 +16,34 @@ const DATA = {
                     title: "GRID: Ultimate Edition",
                     version: "v1.0.118.9362 + 8 DLCs",
                     size: "37.1 GB",
-                    seeders: 3,
-                    leechers: 4,
+                    seeders: 10,
+                    leechers: 9,
                     magnets: [
-                        "magnet:?xt=urn:btih:7A2C461163B86AA9E79D8E406FB24FB721841E02&dn=GRID%3A+Ultimate+Edition+%28v1.0.118.9362+%2B+8+DLCs%2C+MULTi7%29+%5BFitGirl+Repack%2C+Selective+Download+-+from+37.1+GB%5D&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969"
+                        "magnet:?xt=urn:btih:7A2C461163B86AA9E79D8E406FB24FB721841E02&dn=GRID%3A+Ultimate+Edition+%28v1.0.118.9362+%2B+8+DLCs%2C+MULTi7%29+%5BFitGirl+Repack%2C+Selective+Download+-+from+37.1+GB%5D&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969&tr=udp%3A%2F%2F9.rarbg.me%3A2720%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2800%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.piratepublic.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337&tr=udp%3A%2F%2F9.rarbg.com%3A2710%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker1.wasabii.com.tw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce"
                     ],
-                    torrents: [
-                        "./Torrents/GRID.torrent"
-                    ]
+                    torrents: null
                 },
                 {
                     title: "Grand Theft Auto: The Trilogy",
                     version: "The Definitive Edition v1.112.48699928",
                     size: "22.8 GB",
-                    seeders: 943,
-                    leechers: 79,
+                    seeders: 1066,
+                    leechers: 88,
                     magnets: [
-                        "magnet:?xt=urn:btih:A9154E52E835B89043CD5FDFAAD3008D1987A44C&dn=Grand+Theft+Auto%3A+The+Trilogy"
+                        "magnet:?xt=urn:btih:A9154E52E835B89043CD5FDFAAD3008D1987A44C&dn=Grand+Theft+Auto%3A+The+Trilogy+-+The+Definitive+Edition+%28v1.112.48699928%2C+MULTi13%29+%5BFitGirl+Repack%5D&tr=udp%3A%2F%2Fopentor.net%3A6969&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.theoks.net%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.ccp.ovh%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=https%3A%2F%2Ftracker.tamersunion.org%3A443%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.bt4g.com%3A2095%2Fannounce&tr=udp%3A%2F%2Fbt2.archive.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fbt1.archive.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.filemail.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker1.bt.moack.co.kr%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce"
                     ],
-                    torrents: [
-                        "./Torrents/GTA_Trilogy.torrent"
-                    ]
-                },
-                {
-                    title: "Tom Clancy's Ghost Recon: Wildlands",
-                    version: "Ultimate Edition (v4792145 build 5948128 + All DLCs + MULTi16)",
-                    size: "33.2 GB",
-                    seeders: 20,
-                    leechers: 244,
-                    magnets: [
-                        "magnet:?xt=urn:btih:18cbe74b9ad52560d959b72fff779c1edcb5b221&dn=Tom%20Clancys%20Ghost%20Recon%20Wildlands"
-                    ],
-                    torrents: [
-                        "./Torrents/Ghost_Recon_Wildlands.torrent"
-                    ]
+                    torrents: null
                 },
                 {
                     title: "Watch Dogs: Legion",
                     version: "Ultimate Edition (v1.5.6 + Win 11 Fix + All DLCs + HD Textures Pack + 4K Videos + MULTi15)",
-                    size: "40 GB",
-                    seeders: 49,
-                    leechers: 48,
+                    size: "40-110 GB",
+                    seeders: 220,
+                    leechers: 657,
                     magnets: [
-                        "magnet:?xt=urn:btih:bf9c9997347f2e08f96190228b52eecbf1a2d429&dn=Watch%20Dogs%20Legion"
+                        "magnet:?xt=urn:btih:BF9C9997347F2E08F96190228B52EECBF1A2D429&dn=Watch+Dogs%3A+Legion+-+Ultimate+Edition+%28v1.5.6+%2B+All+DLCs+%2B+HD+Textures+Pack+%2B+4K+Videos+%2B+MULTi15%29+%28From+40+GB%29+%28Fast+Install%29+-+%5BEMPRESS+%2F+DODI+Repack%5D&tr=udp%3A%2F%2F9.rarbg.to%3A2870%2Fannounce&tr=udp%3A%2F%2Feddie4.nl%3A6969%2Fannounce&tr=udp%3A%2F%2Fshadowshq.yi.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fthetracker.org%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.piratepublic.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337&tr=udp%3A%2F%2F9.rarbg.com%3A2710%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker1.wasabii.com.tw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.cypherpunks.ru%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce"
                     ],
                     torrents: [
-                        "./Torrents/Watch_Dogs_Legion.torrent",
                         "./Torrents/Watch_Dogs_Legion_4K_Pack.torrent"
                     ]
                 }
@@ -74,53 +56,59 @@ const DATA = {
                     title: "S05E01",
                     version: "2160p NF WEB-DL DDP5 1 Atmos DV H 265-FLUX",
                     size: "9.82 GB",
-                    seeders: 95,
-                    leechers: 217,
+                    seeders: 92,
+                    leechers: 164,
                     magnets: [
-                        "magnet:?xt=urn:btih:0081ed185cd2fa1eba858256f9f908b45eeb4462&dn=Stranger+Things+S05E01"
+                        "magnet:?xt=urn:btih:0081ed185cd2fa1eba858256f9f908b45eeb4462&dn=Stranger+Things+S05E01+Chapter+One+The+Crawl+2160p+NF+WEB-DL+DDP5+1+Atmos+DV+H+265-FLUX+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce"
                     ],
-                    torrents: [
-                        "./Torrents/Stranger_Things_S05E01.torrent"
-                    ]
+                    torrents: null
                 },
                 {
                     title: "S05E02",
                     version: "2160p NF WEB-DL DDP5 1 DV H 265-NTb",
                     size: "7.98GB",
-                    seeders: 171,
-                    leechers: 187,
+                    seeders: 153,
+                    leechers: 119,
                     magnets: [
-                        "magnet:?xt=urn:btih:d8a6717549f0bb6560dbcc41595cdcba525aa6e5&dn=Stranger+Things+S05E02"
+                        "magnet:?xt=urn:btih:d8a6717549f0bb6560dbcc41595cdcba525aa6e5&dn=Stranger+Things+S05E02+Chapter+Two+The+Vanishing+of+2160p+NF+WEB-DL+DDP5+1+DV+H+265-NTb+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce"
                     ],
-                    torrents: [
-                        "./Torrents/Stranger_Things_S05E02.torrent"
-                    ]
+                    torrents: null
                 },
                 {
                     title: "S05E03",
                     version: "2160p NF WEB-DL DDP5 1 DV H 265-NTb",
                     size: "9.42GB",
-                    seeders: 384,
-                    leechers: 217,
+                    seeders: 330,
+                    leechers: 139,
                     magnets: [
-                        "magnet:?xt=urn:btih:0ed07a25a9373e49d7d183a4fc0112f1a0eb9d4a&dn=Stranger+Things+S05E03"
+                        "magnet:?xt=urn:btih:0ed07a25a9373e49d7d183a4fc0112f1a0eb9d4a&dn=Stranger+Things+S05E03+Chapter+Three+The+Turnbow+Trap+2160p+NF+WEB-DL+DDP5+1+DV+H+265-NTb+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce"
                     ],
-                    torrents: [
-                        "./Torrents/Stranger_Things_S05E03.torrent"
-                    ]
+                    torrents: null
                 },
                 {
                     title: "S05E04",
                     version: "2160p NF WEB-DL DDP5 1 Atmos DV HDR H 265-FLUX",
                     size: "12.12GB",
-                    seeders: 180,
-                    leechers: 140,
+                    seeders: 156,
+                    leechers: 97,
                     magnets: [
-                        "magnet:?xt=urn:btih:469311b08446144541ec8ade889a35d777fcb396&dn=Stranger+Things+S05E04"
+                        "magnet:?xt=urn:btih:469311b08446144541ec8ade889a35d777fcb396&dn=Stranger+Things+S05E04+Chapter+Four+Sorcerer+2160p+NF+WEB-DL+DDP5+1+Atmos+DV+HDR+H+265-FLUX+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce"
                     ],
-                    torrents: [
-                        "./Torrents/Stranger_Things_S05E04.torrent"
-                    ]
+                    torrents: null
+                }
+            ]
+        },
+        {
+            name: "Movies",
+            items: [
+                {
+                    title: "Tron: Ares (2025)",
+                    version: "Waiting till Jan 9 for HDR",
+                    size: null,
+                    seeders: null,
+                    leechers: null,
+                    magnets: null,
+                    torrents: null
                 }
             ]
         }
