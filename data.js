c9c454eee85fc501edd141df303d1be461d2c608
leonardodastin <leonardodastin@gmail.com>
2025-12-02 22:30:21 +0530

    Dec2-1

--- data.js
+++ data.js
@@ -21,7 +21,7 @@ const DATA = {
                     size: "22.8 GB",
                     seeders: 1066,
                     leechers: 88,
-                    magnets: ["magnet:?xt=urn:btih:A9154E52E835B89043CD5FDFAAD3008D1987A44C&dn=GTA"],
+                    magnets: [String.raw`magnet:?xt=urn:btih:A9154E52E835B89043CD5FDFAAD3008D1987A44C&dn=Grand+Theft+Auto%3A+The+Trilogy+-+The+Definitive+Edition+%28v1.112.48699928%2C+MULTi13%29+%5BFitGirl+Repack%5D&tr=udp%3A%2F%2Fopentor.net%3A6969&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.theoks.net%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.ccp.ovh%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=https%3A%2F%2Ftracker.tamersunion.org%3A443%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.bt4g.com%3A2095%2Fannounce&tr=udp%3A%2F%2Fbt2.archive.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fbt1.archive.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.filemail.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker1.bt.moack.co.kr%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce`],
                     torrents: null
                 },
                 {
@@ -31,7 +31,7 @@ const DATA = {
                     size: "40-110 GB",
                     seeders: 220,
                     leechers: 657,
-                    magnets: ["magnet:?xt=urn:btih:BF9C9997347F2E08F96190228B52EECBF1A2D429&dn=WatchDogs"],
+                    magnets: [String.raw`magnet:?xt=urn:btih:BF9C9997347F2E08F96190228B52EECBF1A2D429&dn=Watch+Dogs%3A+Legion+-+Ultimate+Edition+%28v1.5.6+%2B+All+DLCs+%2B+HD+Textures+Pack+%2B+4K+Videos+%2B+MULTi15%29+%28From+40+GB%29+%28Fast+Install%29+-+%5BEMPRESS+%2F+DODI+Repack%5D&tr=udp%3A%2F%2F9.rarbg.to%3A2870%2Fannounce&tr=udp%3A%2F%2Feddie4.nl%3A6969%2Fannounce&tr=udp%3A%2F%2Fshadowshq.yi.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fthetracker.org%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.piratepublic.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337&tr=udp%3A%2F%2F9.rarbg.com%3A2710%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker1.wasabii.com.tw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.cypherpunks.ru%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce`],
                     torrents: ["./Torrents/Watch_Dogs_Legion_4K_Pack.torrent"]
                 }
             ]
@@ -46,7 +46,7 @@ const DATA = {
                     size: "9.82 GB",
                     seeders: 92,
                     leechers: 164,
-                    magnets: ["magnet:?xt=urn:btih:0081ed185cd2fa1eba858256f9f908b45eeb4462&dn=ST_S05E01"],
+                    magnets: [String.raw``],
                     torrents: null
                 },
                 {
@@ -56,7 +56,7 @@ const DATA = {
                     size: "7.98GB",
                     seeders: 153,
                     leechers: 119,
-                    magnets: ["magnet:?xt=urn:btih:d8a6717549f0bb6560dbcc41595cdcba525aa6e5&dn=ST_S05E02"],
+                    magnets: [String.raw``],
                     torrents: null
                 },
                 {
@@ -66,7 +66,7 @@ const DATA = {
                     size: "9.42GB",
                     seeders: 330,
                     leechers: 139,
-                    magnets: ["magnet:?xt=urn:btih:0ed07a25a9373e49d7d183a4fc0112f1a0eb9d4a&dn=ST_S05E03"],
+                    magnets: [String.raw``],
                     torrents: null
                 },
                 {
@@ -76,7 +76,7 @@ const DATA = {
                     size: "12.12GB",
                     seeders: 156,
                     leechers: 97,
-                    magnets: ["magnet:?xt=urn:btih:469311b08446144541ec8ade889a35d777fcb396&dn=ST_S05E04"],
+                    magnets: [String.raw``],
                     torrents: null
                 }
             ]
@@ -91,7 +91,7 @@ const DATA = {
                     size: "9.42GB",
                     seeders: 330,
                     leechers: 139,
-                    magnets: ["magnet:?xt=urn:btih:518eb6e54fbd7bc2ebc022697ea6e056482602ff&dn=Frankenstein"],
+                    magnets: [String.raw``],
                     torrents: null
                 },
                 {
