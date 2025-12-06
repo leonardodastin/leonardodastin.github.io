84bcaefe5bf7235a73e2b6e9b5913a908bb191f8
leonardodastin <leonardodastin@gmail.com>
2025-12-06 20:29:08 +0530

    Dec6

--- data.js
+++ data.js
@@ -12,7 +12,7 @@ const DATA = {
                     seeders: 2460,
                     leechers: 5527,
                     magnets: [String.raw`magnet:?xt=urn:btih:f665ecb9a834b6c3935e9c4baf672fe3ce5872b7&dn=Pluribus+S01E06+HDP+2160p+ATVP+WEB-DL+DDP5+1+DV+HDR+H+265-NTb+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`],
-                    torrents: [null]
+                    torrents: null
                 },
                 {
                     id: "shows-2",
@@ -22,7 +22,7 @@ const DATA = {
                     seeders: null,
                     leechers: null,
                     magnets: [String.raw`https://1337x.to/user/showstopper/`],
-                    torrents: [null]
+                    torrents: null
                 }
             ]
         }
