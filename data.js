81d4a1bac96e4d3631f52145c0876a8d1181db73
leonardodastin <leonardodastin@gmail.com>
2025-12-06 20:22:22 +0530

    Dec6

--- data.js
+++ data.js
@@ -2,52 +2,27 @@
 const DATA = {
     categories: [
         {
-            name: "Games",
+            name: "Shows",
             items: [
                 {
-                    id: "game-3",
-                    title: "Grand Theft Auto: The Trilogy",
-                    version: "The Definitive Edition v1.112.48699928",
-                    size: "22.8 GB",
-                    seeders: 1066,
-                    leechers: 88,
-                    magnets: [String.raw`magnet:?xt=urn:btih:A9154E52E835B89043CD5FDFAAD3008D1987A44C&dn=Grand+Theft+Auto%3A+The+Trilogy+-+The+Definitive+Edition+%28v1.112.48699928%2C+MULTi13%29+%5BFitGirl+Repack%5D&tr=udp%3A%2F%2Fopentor.net%3A6969&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.theoks.net%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.ccp.ovh%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=https%3A%2F%2Ftracker.tamersunion.org%3A443%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.bt4g.com%3A2095%2Fannounce&tr=udp%3A%2F%2Fbt2.archive.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fbt1.archive.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.filemail.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker1.bt.moack.co.kr%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce`],
-                    torrents: null
+                    id: "shows-1",
+                    title: "Pluribus S01E06",
+                    version: "HDP 2160p ATVP WEB-DL DDP5 1 DV HDR H 265-NTb",
+                    size: "8.73GB",
+                    seeders: 2460,
+                    leechers: 5527,
+                    magnets: [String.raw`magnet:?xt=urn:btih:f665ecb9a834b6c3935e9c4baf672fe3ce5872b7&dn=Pluribus+S01E06+HDP+2160p+ATVP+WEB-DL+DDP5+1+DV+HDR+H+265-NTb+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`],
+                    torrents: [null]
                 },
                 {
-                    id: "game-4",
-                    title: "Watch Dogs: Legion",
-                    version: "Ultimate Edition (v1.5.6 + Win 11 Fix + All DLCs)",
-                    size: "40-110 GB",
-                    seeders: 220,
-                    leechers: 657,
-                    magnets: [String.raw`magnet:?xt=urn:btih:BF9C9997347F2E08F96190228B52EECBF1A2D429&dn=Watch+Dogs%3A+Legion+-+Ultimate+Edition+%28v1.5.6+%2B+All+DLCs+%2B+HD+Textures+Pack+%2B+4K+Videos+%2B+MULTi15%29+%28From+40+GB%29+%28Fast+Install%29+-+%5BEMPRESS+%2F+DODI+Repack%5D&tr=udp%3A%2F%2F9.rarbg.to%3A2870%2Fannounce&tr=udp%3A%2F%2Feddie4.nl%3A6969%2Fannounce&tr=udp%3A%2F%2Fshadowshq.yi.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fthetracker.org%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.piratepublic.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337&tr=udp%3A%2F%2F9.rarbg.com%3A2710%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker1.wasabii.com.tw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.cypherpunks.ru%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce`],
-                    torrents: ["./Torrents/Watch_Dogs_Legion_4K_Pack.torrent"]
-                }
-            ]
-        },
-        {
-            name: "Movies",
-            items: [
-                {
-                    id: "movie-1",
-                    title: "Frankenstein",
-                    version: "2160p.NF.WEB-DL.HDR.MULTi.DDP5.1.Atmos.H265",
-                    size: "9.42GB",
-                    seeders: 330,
-                    leechers: 139,
-                    magnets: [String.raw`magnet:?xt=urn:btih:B05657AB241DE081EB00A61E49A1DB0B6EB3CBB5&dn=Frankenstein.2025.2160p.Hybrid.MULTI.WEB-DL.DV.HDR.H265-AOC&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.birkenwald.de%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.moeking.me%3A6969%2Fannounce&tr=udp%3A%2F%2Fipv4.tracker.harry.lu%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce`],
-                    torrents: ["./Torrents/Frankenstein_2025.torrent"]
-                },
-                {
-                    id: "movie-2",
-                    title: "Tron: Ares (2025)",
-                    version: "Hybrid.2160p.WEB-DL.DV.HDR.DDP5.1.Atmos.H265-AOC ",
-                    size: "20.38 GB",
-                    seeders: 1165,
-                    leechers: 578,
-                    magnets: [String.raw`magnet:?xt=urn:btih:522DC4F30C6E027173BCD813F607B01EE127D13C&dn=Tron.Ares.2025.Hybrid.2160p.WEB-DL.DV.HDR.DDP5.1.Atmos.H265-AOC&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.birkenwald.de%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.moeking.me%3A6969%2Fannounce&tr=udp%3A%2F%2Fipv4.tracker.harry.lu%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce`],
-                    torrents: ["./Torrents/Tron_Ares_2025.torrent"]
+                    id: "shows-2",
+                    title: "Formula 1",
+                    version: null,
+                    size: null,
+                    seeders: null,
+                    leechers: null,
+                    magnets: [String.raw`https://1337x.to/user/showstopper/`],
+                    torrents: [null]
                 }
             ]
         }
@@ -63,4 +38,4 @@ window.downloadedData = downloadedData;
 window.expandedCategory = expandedCategory;
 window.showDownloaded = showDownloaded;
 
-// Date changed Dec 3 09:57 PM
+// Date changed Dec 6 08:22 PM
