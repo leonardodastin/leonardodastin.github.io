2600523a7d8ab1e8c4ffcd9ce01bb92d2dc5cf23
leonardodastin <leonardodastin@gmail.com>
2026-01-07 00:19:35 +0530

    Jan 7 - 2

--- data.js
+++ data.js
@@ -12,17 +12,7 @@ const DATA = {
                     seeders: 693,
                     leechers: 1045,
                     magnets: [String.raw`magnet:?xt=urn:btih:9c652e73c46947b41384594c92dc4996b919583e&dn=Predator.Badlands.2025.2160p.iT.WEB-DL.DV.HDR10 .MULTi.DDP5.1.Atmos.H265.MP4-BEN.THE.MEN&xl=24591767116&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://tracker.opentrackr.org:1337&tr=udp://open.stealth.si:80/announce&tr=udp://public.demonoid.ch:6969/announce&tr=udp://explodie.org:6969/announce&tr=udp://open.demonii.com:1337/announce&tr=udp://open.demonoid.ch:6969/announce&tr=http://bvarf.tracker.sh:2086/announce&tr=http://tracker.renfei.net:8080/announce&tr=https://tracker.pmman.tech:443/announce&tr=udp://bandito.byterunner.io:6969/announce&tr=udp://d40969.acod.regrucolo.ru:6969/announc&tr=http://tracker.bt4g.com:2095/announce&tr=http://bt.okmp3.ru:2710/announce&tr=udp://tracker.torrent.eu.org:451/announce`],
-                    torrents: 'Torrents/Predator.Badlands.2025.torrent'
-                },
-                {
-                    id: "shows-2",
-                    title: "Formula 1",
-                    version: null,
-                    size: null,
-                    seeders: null,
-                    leechers: null,
-                    magnets: [String.raw`https://1337x.to/user/showstopper/`],
-                    torrents: null
+                    torrents: ['Torrents/Predator.Badlands.2025.torrent']
                 }
             ]
         }
@@ -38,4 +28,4 @@ window.downloadedData = downloadedData;
 window.expandedCategory = expandedCategory;
 window.showDownloaded = showDownloaded;
 
-// Date changed Jan 7 12:15 PM
+// Date changed Jan 7 12:19 PM [II]
