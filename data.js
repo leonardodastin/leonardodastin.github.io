fcbb453ea30a07c05b3c7c0456a7359af9cf687c
leonardodastin <leonardodastin@gmail.com>
2026-03-11 22:44:14 +0530

    TedS02

--- data.js
+++ data.js
@@ -1,5 +1,4 @@
-// data.js
-const DATA = {
+/*const DATA = {
     categories: [
         {
             name: "Movies",
@@ -13,6 +12,113 @@ const DATA = {
                     leechers: 1045,
                     magnets: [String.raw`magnet:?xt=urn:btih:9c652e73c46947b41384594c92dc4996b919583e&dn=Predator.Badlands.2025.2160p.iT.WEB-DL.DV.HDR10 .MULTi.DDP5.1.Atmos.H265.MP4-BEN.THE.MEN&xl=24591767116&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://tracker.opentrackr.org:1337&tr=udp://open.stealth.si:80/announce&tr=udp://public.demonoid.ch:6969/announce&tr=udp://explodie.org:6969/announce&tr=udp://open.demonii.com:1337/announce&tr=udp://open.demonoid.ch:6969/announce&tr=http://bvarf.tracker.sh:2086/announce&tr=http://tracker.renfei.net:8080/announce&tr=https://tracker.pmman.tech:443/announce&tr=udp://bandito.byterunner.io:6969/announce&tr=udp://d40969.acod.regrucolo.ru:6969/announc&tr=http://tracker.bt4g.com:2095/announce&tr=http://bt.okmp3.ru:2710/announce&tr=udp://tracker.torrent.eu.org:451/announce`],
                     torrents: ['Torrents/Predator.Badlands.2025.torrent']
+                }
+            ]
+        }
+    ]
+};*/
+
+// data.js
+const DATA = {
+    categories: [
+        {
+            name: "Ted S02",
+            items: [
+                {
+                    id: "td-1",
+                    title: "Ted S02E01",
+                    version: "1080p WEB h264-ETHEL",
+                    size: "1.65 GB",
+                    seeders: 1500,
+                    leechers: 1500,
+                    magnets: [String.raw`magnet:?xt=urn:btih:18d5f1c11b92fa758edee05c783ce5e1cef3d2c9&dn=Ted+S02E01+1080p+WEB+h264-ETHEL+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`,
+                        String.raw`magnet:?xt=urn:btih:18D5F1C11B92FA758EDEE05C783CE5E1CEF3D2C9&dn=Ted.S02E01.1080p.WEB.h264-ETHEL&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=udp://9.rarbg.me:2850/announce&tr=udp://9.rarbg.to:2920/announce&tr=udp://tracker.opentrackr.org:1337&tr=udp://tracker.leechers-paradise.org:6969/announce`
+                    ],
+                    torrents: [null]
+                },
+                {
+                    id: "td-2",
+                    title: "Ted S02E02",
+                    version: "1080p WEB h264-ETHEL",
+                    size: "1.65 GB",
+                    seeders: 1500,
+                    leechers: 1500,
+                    magnets: [String.raw`magnet:?xt=urn:btih:a0156ad2371e32d6d7dcc113b2ef817d6b9672b5&dn=Ted+S02E02+1080p+WEB+h264-ETHEL+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`,
+                        String.raw`magnet:?xt=urn:btih:A0156AD2371E32D6D7DCC113B2EF817D6B9672B5&dn=Ted.S02E02.1080p.WEB.h264-ETHEL&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=udp://9.rarbg.me:2850/announce&tr=udp://9.rarbg.to:2920/announce&tr=udp://tracker.opentrackr.org:1337&tr=udp://tracker.leechers-paradise.org:6969/announce`
+                    ],
+                    torrents: [null]
+                },
+                                {
+                    id: "td-3",
+                    title: "Ted S02E03",
+                    version: "1080p WEB h264-ETHEL",
+                    size: "1.65 GB",
+                    seeders: 1500,
+                    leechers: 1500,
+                    magnets: [String.raw`magnet:?xt=urn:btih:b5b41d788d0cef20628ab98427ec61ae23b1bb14&dn=Ted+S02E03+1080p+WEB+h264-ETHEL+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`,
+                        String.raw`magnet:?xt=urn:btih:B5B41D788D0CEF20628AB98427EC61AE23B1BB14&dn=Ted.S02E03.1080p.WEB.h264-ETHEL&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=udp://9.rarbg.me:2850/announce&tr=udp://9.rarbg.to:2920/announce&tr=udp://tracker.opentrackr.org:1337&tr=udp://tracker.leechers-paradise.org:6969/announce`
+                    ],
+                    torrents: [null]
+                },
+                {
+                    id: "td-4",
+                    title: "Ted S02E04",
+                    version: "1080p WEB h264-ETHEL",
+                    size: "1.65 GB",
+                    seeders: 1500,
+                    leechers: 1500,
+                    magnets: [String.raw`magnet:?xt=urn:btih:2e3e097592b31c00c6d6d7addaebfbed56351c86&dn=Ted+S02E04+1080p+WEB+h264-ETHEL+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`,
+                        String.raw`magnet:?xt=urn:btih:2E3E097592B31C00C6D6D7ADDAEBFBED56351C86&dn=Ted.S02E04.1080p.WEB.h264-ETHEL&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=udp://9.rarbg.me:2850/announce&tr=udp://9.rarbg.to:2920/announce&tr=udp://tracker.opentrackr.org:1337&tr=udp://tracker.leechers-paradise.org:6969/announce`
+                    ],
+                    torrents: [null]
+                },
+                                {
+                    id: "td-5",
+                    title: "Ted S02E05",
+                    version: "1080p WEB h264-ETHEL",
+                    size: "1.65 GB",
+                    seeders: 1500,
+                    leechers: 1500,
+                    magnets: [String.raw`magnet:?xt=urn:btih:757f0f8e437d66ef542f31636d1cdaed14a3a1ff&dn=Ted+S02E05+1080p+WEB+h264-ETHEL+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`,
+                        String.raw`magnet:?xt=urn:btih:757F0F8E437D66EF542F31636D1CDAED14A3A1FF&dn=Ted.S02E05.1080p.WEB.h264-ETHEL&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=udp://9.rarbg.me:2850/announce&tr=udp://9.rarbg.to:2920/announce&tr=udp://tracker.opentrackr.org:1337&tr=udp://tracker.leechers-paradise.org:6969/announce`
+                    ],
+                    torrents: [null]
+                },
+                {
+                    id: "td-6",
+                    title: "Ted S02E06",
+                    version: "1080p WEB h264-ETHEL",
+                    size: "1.65 GB",
+                    seeders: 1500,
+                    leechers: 1500,
+                    magnets: [String.raw`magnet:?xt=urn:btih:3A294FA22490813967C5ED1BC13512B6C42F7975&dn=Ted+S02E06+1080p+WEB+h264-ETHEL+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`,
+                        String.raw`magnet:?xt=urn:btih:3a294fa22490813967c5ed1bc13512b6c42f7975&dn=Ted.S02E06.1080p.WEB.h264-ETHEL[eztvx.to]&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://open.stealth.si:80/announce&tr=udp://tracker.torrent.eu.org:451/announce&tr=udp://tracker.dler.org:6969/announce&tr=https://tracker.moeblog.cn:443/announce&tr=https://tracker.zhuqiy.com:443/announce&tr=udp://open.dstud.io:6969/announce`
+                    ],
+                    torrents: [null]
+                },
+                                {
+                    id: "td-7",
+                    title: "Ted S02E07",
+                    version: "1080p WEB h264-ETHEL",
+                    size: "1.65 GB",
+                    seeders: 1500,
+                    leechers: 1500,
+                    magnets: [String.raw`magnet:?xt=urn:btih:bfba5e2c7033bdf37b84838fb03373028b4bd9cc&dn=Ted+S02E07+1080p+WEB+h264-ETHEL+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`,
+                        String.raw`magnet:?xt=urn:btih:BFBA5E2C7033BDF37B84838FB03373028B4BD9CC&dn=Ted.S02E07.1080p.WEB.h264-ETHEL&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=udp://9.rarbg.me:2850/announce&tr=udp://9.rarbg.to:2920/announce&tr=udp://tracker.opentrackr.org:1337&tr=udp://tracker.leechers-paradise.org:6969/announce`
+                    ],
+                    torrents: [null]
+                },
+                {
+                    id: "td-8",
+                    title: "Ted S02E08",
+                    version: "1080p WEB h264-ETHEL",
+                    size: "1.65 GB",
+                    seeders: 1500,
+                    leechers: 1500,
+                    magnets: [String.raw`magnet:?xt=urn:btih:E2B4E3916B18B90D35671C0AF891AE7686827B98&dn=Ted+S02E08+1080p+WEB+h264-ETHEL+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`,
+                        String.raw`magnet:?xt=urn:btih:e2b4e3916b18b90d35671c0af891ae7686827b98&dn=Ted.S02E08.1080p.WEB.h264-ETHEL[eztvx.to]&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://open.stealth.si:80/announce&tr=udp://tracker.torrent.eu.org:451/announce&tr=udp://tracker.dler.org:6969/announce&tr=https://tracker.moeblog.cn:443/announce&tr=https://tracker.zhuqiy.com:443/announce&tr=udp://open.dstud.io:6969/announce`
+                    ],
+                    torrents: [null]
                 }
             ]
         }
