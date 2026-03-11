52b7cee5ad23c997d76b10e7f02f1aa0d749684e
leonardodastin <leonardodastin@gmail.com>
2026-03-12 01:05:20 +0530

    One Piece S02

--- data.js
+++ data.js
@@ -14,6 +14,21 @@
                     torrents: ['Torrents/Predator.Badlands.2025.torrent']
                 }
             ]
+        },
+        {
+            name: "Movies 2",
+            items: [
+                {
+                    id: "mv-2",
+                    title: "Predator.Badlands.2025",
+                    version: "2160p.iT.WEB-DL.DV.HDR10+.MULTi.DDP5.1.Atmos.H265.MP4-BEN.THE.MEN",
+                    size: "22.9GB",
+                    seeders: 693,
+                    leechers: 1045,
+                    magnets: [String.raw`magnet:?xt=urn:btih:9c652e73c46947b41384594c92dc4996b919583e&dn=Predator.Badlands.2025.2160p.iT.WEB-DL.DV.HDR10 .MULTi.DDP5.1.Atmos.H265.MP4-BEN.THE.MEN&xl=24591767116&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://tracker.opentrackr.org:1337&tr=udp://open.stealth.si:80/announce&tr=udp://public.demonoid.ch:6969/announce&tr=udp://explodie.org:6969/announce&tr=udp://open.demonii.com:1337/announce&tr=udp://open.demonoid.ch:6969/announce&tr=http://bvarf.tracker.sh:2086/announce&tr=http://tracker.renfei.net:8080/announce&tr=https://tracker.pmman.tech:443/announce&tr=udp://bandito.byterunner.io:6969/announce&tr=udp://d40969.acod.regrucolo.ru:6969/announc&tr=http://tracker.bt4g.com:2095/announce&tr=http://bt.okmp3.ru:2710/announce&tr=udp://tracker.torrent.eu.org:451/announce`],
+                    torrents: null
+                }
+            ]
         }
     ]
 };*/
@@ -22,102 +37,86 @@
 const DATA = {
     categories: [
         {
-            name: "Ted S02",
+            name: "One Piece S02",
             items: [
                 {
-                    id: "td-1",
-                    title: "Ted S02E01",
-                    version: "1080p WEB h264-ETHEL",
+                    id: "op-1",
+                    title: "One Piece S02E01",
+                    version: "2160p NF WEB-DL DDP5 1 Atmos DV HDR H 265-BiOMA",
                     size: "1.65 GB",
                     seeders: 1500,
                     leechers: 1500,
-                    magnets: [String.raw`magnet:?xt=urn:btih:18d5f1c11b92fa758edee05c783ce5e1cef3d2c9&dn=Ted+S02E01+1080p+WEB+h264-ETHEL+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`,
-                        String.raw`magnet:?xt=urn:btih:18D5F1C11B92FA758EDEE05C783CE5E1CEF3D2C9&dn=Ted.S02E01.1080p.WEB.h264-ETHEL&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=udp://9.rarbg.me:2850/announce&tr=udp://9.rarbg.to:2920/announce&tr=udp://tracker.opentrackr.org:1337&tr=udp://tracker.leechers-paradise.org:6969/announce`
-                    ],
+                    magnets: [String.raw`magnet:?xt=urn:btih:3b1522f474919cfa46b8c1af8d6042866a9722bb&dn=ONE+PIECE+2023+S02E01+2160p+NF+WEB-DL+DDP5+1+Atmos+DV+HDR+H+265-BiOMA+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`],
                     torrents: null
                 },
                 {
-                    id: "td-2",
-                    title: "Ted S02E02",
-                    version: "1080p WEB h264-ETHEL",
+                    id: "op-2",
+                    title: "One Piece S02E02",
+                    version: "2160p NF WEB-DL DDP5 1 Atmos DV HDR H 265-BiOMA",
                     size: "1.65 GB",
                     seeders: 1500,
                     leechers: 1500,
-                    magnets: [String.raw`magnet:?xt=urn:btih:a0156ad2371e32d6d7dcc113b2ef817d6b9672b5&dn=Ted+S02E02+1080p+WEB+h264-ETHEL+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`,
-                        String.raw`magnet:?xt=urn:btih:A0156AD2371E32D6D7DCC113B2EF817D6B9672B5&dn=Ted.S02E02.1080p.WEB.h264-ETHEL&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=udp://9.rarbg.me:2850/announce&tr=udp://9.rarbg.to:2920/announce&tr=udp://tracker.opentrackr.org:1337&tr=udp://tracker.leechers-paradise.org:6969/announce`
-                    ],
+                    magnets: [String.raw`magnet:?xt=urn:btih:a50fd36699158187459f65b89227eae9f605c372&dn=ONE+PIECE+2023+S02E02+2160p+NF+WEB-DL+DDP5+1+Atmos+DV+HDR+H+265-BiOMA+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`],
+                    torrents: null
+                },
+                {
+                    id: "op-3",
+                    title: "One Piece S02E03",
+                    version: "2160p NF WEB-DL DDP5 1 Atmos DV HDR H 265-BiOMA",
+                    size: "1.65 GB",
+                    seeders: 1500,
+                    leechers: 1500,
+                    magnets: [String.raw`magnet:?xt=urn:btih:a69a3e69fb304b382630ae4de663e2c36019df23&dn=ONE+PIECE+2023+S02E03+2160p+NF+WEB-DL+DDP5+1+Atmos+DV+HDR+H+265-BiOMA+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`],
+                    torrents: null
+                },
+                {
+                    id: "op-4",
+                    title: "One Piece S02E04",
+                    version: "2160p NF WEB-DL DDP5 1 Atmos DV HDR H 265-BiOMA",
+                    size: "1.65 GB",
+                    seeders: 1500,
+                    leechers: 1500,
+                    magnets: [String.raw`magnet:?xt=urn:btih:c41564653c9e40f67b45aaf70f4d25eb9d5c6255&dn=ONE+PIECE+2023+S02E04+2160p+NF+WEB-DL+DDP5+1+Atmos+DV+HDR+H+265-BiOMA+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`],
                     torrents: null
                 },
                                 {
-                    id: "td-3",
-                    title: "Ted S02E03",
-                    version: "1080p WEB h264-ETHEL",
+                    id: "op-5",
+                    title: "One Piece S02E05",
+                    version: "2160p NF WEB-DL DDP5 1 Atmos DV HDR H 265-BiOMA",
                     size: "1.65 GB",
                     seeders: 1500,
                     leechers: 1500,
-                    magnets: [String.raw`magnet:?xt=urn:btih:b5b41d788d0cef20628ab98427ec61ae23b1bb14&dn=Ted+S02E03+1080p+WEB+h264-ETHEL+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`,
-                        String.raw`magnet:?xt=urn:btih:B5B41D788D0CEF20628AB98427EC61AE23B1BB14&dn=Ted.S02E03.1080p.WEB.h264-ETHEL&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=udp://9.rarbg.me:2850/announce&tr=udp://9.rarbg.to:2920/announce&tr=udp://tracker.opentrackr.org:1337&tr=udp://tracker.leechers-paradise.org:6969/announce`
-                    ],
+                    magnets: [String.raw`magnet:?xt=urn:btih:bc2a33a53b0e1b8fd631bc2b8b2a381818ccea64&dn=ONE+PIECE+2023+S02E05+2160p+NF+WEB-DL+DDP5+1+Atmos+DV+HDR+H+265-BiOMA+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`],
                     torrents: null
                 },
                 {
-                    id: "td-4",
-                    title: "Ted S02E04",
-                    version: "1080p WEB h264-ETHEL",
+                    id: "op-6",
+                    title: "One Piece S02E06",
+                    version: "2160p NF WEB-DL DDP5 1 Atmos DV HDR H 265-BiOMA",
                     size: "1.65 GB",
                     seeders: 1500,
                     leechers: 1500,
-                    magnets: [String.raw`magnet:?xt=urn:btih:2e3e097592b31c00c6d6d7addaebfbed56351c86&dn=Ted+S02E04+1080p+WEB+h264-ETHEL+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`,
-                        String.raw`magnet:?xt=urn:btih:2E3E097592B31C00C6D6D7ADDAEBFBED56351C86&dn=Ted.S02E04.1080p.WEB.h264-ETHEL&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=udp://9.rarbg.me:2850/announce&tr=udp://9.rarbg.to:2920/announce&tr=udp://tracker.opentrackr.org:1337&tr=udp://tracker.leechers-paradise.org:6969/announce`
-                    ],
-                    torrents: null
-                },
-                                {
-                    id: "td-5",
-                    title: "Ted S02E05",
-                    version: "1080p WEB h264-ETHEL",
-                    size: "1.65 GB",
-                    seeders: 1500,
-                    leechers: 1500,
-                    magnets: [String.raw`magnet:?xt=urn:btih:757f0f8e437d66ef542f31636d1cdaed14a3a1ff&dn=Ted+S02E05+1080p+WEB+h264-ETHEL+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`,
-                        String.raw`magnet:?xt=urn:btih:757F0F8E437D66EF542F31636D1CDAED14A3A1FF&dn=Ted.S02E05.1080p.WEB.h264-ETHEL&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=udp://9.rarbg.me:2850/announce&tr=udp://9.rarbg.to:2920/announce&tr=udp://tracker.opentrackr.org:1337&tr=udp://tracker.leechers-paradise.org:6969/announce`
-                    ],
+                    magnets: [String.raw`magnet:?xt=urn:btih:7b505053b67241fe9115844104737171f1227b86&dn=ONE+PIECE+2023+S02E06+2160p+NF+WEB-DL+DDP5+1+Atmos+DV+HDR+H+265-BiOMA+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`],
                     torrents: null
                 },
                 {
-                    id: "td-6",
-                    title: "Ted S02E06",
-                    version: "1080p WEB h264-ETHEL",
+                    id: "op-7",
+                    title: "One Piece S02E07",
+                    version: "2160p NF WEB-DL DDP5 1 Atmos DV HDR H 265-BiOMA",
                     size: "1.65 GB",
                     seeders: 1500,
                     leechers: 1500,
-                    magnets: [String.raw`magnet:?xt=urn:btih:3A294FA22490813967C5ED1BC13512B6C42F7975&dn=Ted+S02E06+1080p+WEB+h264-ETHEL+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`,
-                        String.raw`magnet:?xt=urn:btih:3a294fa22490813967c5ed1bc13512b6c42f7975&dn=Ted.S02E06.1080p.WEB.h264-ETHEL[eztvx.to]&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://open.stealth.si:80/announce&tr=udp://tracker.torrent.eu.org:451/announce&tr=udp://tracker.dler.org:6969/announce&tr=https://tracker.moeblog.cn:443/announce&tr=https://tracker.zhuqiy.com:443/announce&tr=udp://open.dstud.io:6969/announce`
-                    ],
-                    torrents: null
-                },
-                                {
-                    id: "td-7",
-                    title: "Ted S02E07",
-                    version: "1080p WEB h264-ETHEL",
-                    size: "1.65 GB",
-                    seeders: 1500,
-                    leechers: 1500,
-                    magnets: [String.raw`magnet:?xt=urn:btih:bfba5e2c7033bdf37b84838fb03373028b4bd9cc&dn=Ted+S02E07+1080p+WEB+h264-ETHEL+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`,
-                        String.raw`magnet:?xt=urn:btih:BFBA5E2C7033BDF37B84838FB03373028B4BD9CC&dn=Ted.S02E07.1080p.WEB.h264-ETHEL&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=udp://9.rarbg.me:2850/announce&tr=udp://9.rarbg.to:2920/announce&tr=udp://tracker.opentrackr.org:1337&tr=udp://tracker.leechers-paradise.org:6969/announce`
-                    ],
+                    magnets: [String.raw`magnet:?xt=urn:btih:24ee6d4601f0e40f87c5ccb78cbd77b690bbe69b&dn=ONE+PIECE+2023+S02E07+2160p+NF+WEB-DL+DDP5+1+Atmos+DV+HDR+H+265-BiOMA+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`],
                     torrents: null
                 },
                 {
-                    id: "td-8",
-                    title: "Ted S02E08",
-                    version: "1080p WEB h264-ETHEL",
+                    id: "op-8",
+                    title: "One Piece S02E08",
+                    version: "2160p NF WEB-DL DDP5 1 Atmos DV HDR H 265-BiOMA",
                     size: "1.65 GB",
                     seeders: 1500,
                     leechers: 1500,
-                    magnets: [String.raw`magnet:?xt=urn:btih:E2B4E3916B18B90D35671C0AF891AE7686827B98&dn=Ted+S02E08+1080p+WEB+h264-ETHEL+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`,
-                        String.raw`magnet:?xt=urn:btih:e2b4e3916b18b90d35671c0af891ae7686827b98&dn=Ted.S02E08.1080p.WEB.h264-ETHEL[eztvx.to]&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://open.stealth.si:80/announce&tr=udp://tracker.torrent.eu.org:451/announce&tr=udp://tracker.dler.org:6969/announce&tr=https://tracker.moeblog.cn:443/announce&tr=https://tracker.zhuqiy.com:443/announce&tr=udp://open.dstud.io:6969/announce`
-                    ],
+                    magnets: [String.raw`magnet:?xt=urn:btih:41f6ce48ef56314691ae8bb74a6b7276c07da19b&dn=ONE+PIECE+2023+S02E08+2160p+NF+WEB-DL+DDP5+1+Atmos+DV+HDR+H+265-BiOMA+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`],
                     torrents: null
                 }
             ]
