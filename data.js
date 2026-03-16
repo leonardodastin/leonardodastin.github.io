7290ceff164c9b926d68d86f865bd78eecb6b9f8
leonardodastin <leonardodastin@gmail.com>
2026-03-16 16:05:33 +0530

    Dinosaurs

--- data.js
+++ data.js
@@ -16,16 +16,16 @@
             ]
         },
         {
-            name: "Movies 2",
+            name: "Sample",
             items: [
                 {
-                    id: "mv-2",
-                    title: "Predator.Badlands.2025",
-                    version: "2160p.iT.WEB-DL.DV.HDR10+.MULTi.DDP5.1.Atmos.H265.MP4-BEN.THE.MEN",
-                    size: "22.9GB",
-                    seeders: 693,
-                    leechers: 1045,
-                    magnets: [String.raw`magnet:?xt=urn:btih:9c652e73c46947b41384594c92dc4996b919583e&dn=Predator.Badlands.2025.2160p.iT.WEB-DL.DV.HDR10 .MULTi.DDP5.1.Atmos.H265.MP4-BEN.THE.MEN&xl=24591767116&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://tracker.opentrackr.org:1337&tr=udp://open.stealth.si:80/announce&tr=udp://public.demonoid.ch:6969/announce&tr=udp://explodie.org:6969/announce&tr=udp://open.demonii.com:1337/announce&tr=udp://open.demonoid.ch:6969/announce&tr=http://bvarf.tracker.sh:2086/announce&tr=http://tracker.renfei.net:8080/announce&tr=https://tracker.pmman.tech:443/announce&tr=udp://bandito.byterunner.io:6969/announce&tr=udp://d40969.acod.regrucolo.ru:6969/announc&tr=http://tracker.bt4g.com:2095/announce&tr=http://bt.okmp3.ru:2710/announce&tr=udp://tracker.torrent.eu.org:451/announce`],
+                    id: "sa-2",
+                    title: "Title",
+                    version: "Version",
+                    size: "GB",
+                    seeders: 0,
+                    leechers: 0,
+                    magnets: [String.raw``],
                     torrents: null
                 }
             ]
@@ -33,62 +33,9 @@
     ]
 };*/
 
-/*
-        {
-            name: "Scream (franchise) Licdom ita ",
-            items: [
-                {
-                    id: "sc-32",
-                    title: "Scream 2 (1997)",
-                    version: "2160p H265 BluRay Rip 10 bit DV HDR10+ ita AC3 2.0 eng AC3 5.1 sub ita eng NUeng Lic...",
-                    size: "3.4 GB",
-                    seeders: 21,
-                    leechers: 7,
-                    magnets: [String.raw`magnet:?xt=urn:btih:173DF4D2246D1866978383DDF80DE947B554F34E&dn=Scream+2+%281997%29+2160p+H265+BluRay+Rip+10+bit+DV+HDR10%2B+ita+AC3+2.0+eng+AC3+5.1+sub+ita+eng+NUeng+Licdom&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fpublic.popcorn-tracker.org%3A6969%2Fannounce&tr=udp%3A%2F%2F107.150.14.110%3A6969%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2F109.121.134.121%3A1337%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2F114.55.113.60%3A6969%2Fannounce&tr=udp%3A%2F%2F128.199.70.66%3A5944%2Fannounce&tr=udp%3A%2F%2F151.80.120.114%3A2710%2Fannounce&tr=udp%3A%2F%2F168.235.67.63%3A6969%2Fannounce&tr=udp%3A%2F%2F178.33.73.26%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce`],
-                    torrents: null
-                },
-                {
-                    id: "sc-33",
-                    title: "Scream 3 (2000)",
-                    version: "2160p H265 BluRay Rip 10 bit DV HDR10+ ita eng AC3 5.1 sub ita eng NUeng Licdom",
-                    size: "3.1 GB",
-                    seeders: 27,
-                    leechers: 7,
-                    magnets: [String.raw`magnet:?xt=urn:btih:887A580FF345C4D84151EB7211FCE23423642F47&dn=Scream+3+%282000%29+2160p+H265+BluRay+Rip+10+bit+DV+HDR10%2B+ita+eng+AC3+5.1+sub+ita+eng+NUeng+Licdom&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fpublic.popcorn-tracker.org%3A6969%2Fannounce&tr=udp%3A%2F%2F107.150.14.110%3A6969%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2F109.121.134.121%3A1337%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2F114.55.113.60%3A6969%2Fannounce&tr=udp%3A%2F%2F128.199.70.66%3A5944%2Fannounce&tr=udp%3A%2F%2F151.80.120.114%3A2710%2Fannounce&tr=udp%3A%2F%2F168.235.67.63%3A6969%2Fannounce&tr=udp%3A%2F%2F178.33.73.26%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce`],
-                    torrents: null
-                },
-                {
-                    id: "sc-34",
-                    title: "Scream 4 (2011)",
-                    version: "2160p H265 BluRay Rip 10 bit DV HDR10+ ita eng AC3 5.1 sub ita NUita Licdom",
-                    size: "3.4 GB",
-                    seeders: 45,
-                    leechers: 6,
-                    magnets: [String.raw`magnet:?xt=urn:btih:83FDD92B58558175B9D59145BA2E05C7EC41B83F&dn=Scream+4+%282011%29+2160p+H265+BluRay+Rip+10+bit+DV+HDR10%2B+ita+eng+AC3+5.1+sub+ita+NUita+Licdom&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fpublic.popcorn-tracker.org%3A6969%2Fannounce&tr=udp%3A%2F%2F107.150.14.110%3A6969%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2F109.121.134.121%3A1337%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2F114.55.113.60%3A6969%2Fannounce&tr=udp%3A%2F%2F128.199.70.66%3A5944%2Fannounce&tr=udp%3A%2F%2F151.80.120.114%3A2710%2Fannounce&tr=udp%3A%2F%2F168.235.67.63%3A6969%2Fannounce&tr=udp%3A%2F%2F178.33.73.26%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce`],
-                    torrents: null
-                }
-            ]
-        }
-*/
-
 // data.js
 const DATA = {
     categories: [
-        {
-            name: "Formula 1 2026 : R02 ChineseGP",
-            items: [
-                {
-                    id: "fc-4",
-                    title: "Race",
-                    version: "F1TV 4K HLG 2160p",
-                    size: null,
-                    seeders: null,
-                    leechers: null,
-                    magnets: null,
-                    torrents: null
-                }
-            ]
-        },
         {
             name: "Maharaja (2024)",
             items: [
@@ -119,60 +66,51 @@ const DATA = {
             ]
         },
         {
-            name: "Scream franchise QXR (53.3GB)",
+            name: "The Dinosaurs (20.18 GB)",
             items: [
                 {
-                    id: "sq-32",
-                    title: "Scream 2 (1997)",
-                    version: "(2160p BluRay x265 HEVC 10bit HDR AAC 5.1 Tigole) [QxR]",
-                    size: "12 GB",
-                    seeders: 10,
-                    leechers: 12,
-                    magnets: [String.raw`magnet:?xt=urn:btih:28EEC6DC01F736386BEBFEA909E78407DDE37D0A&dn=Scream+2+%281997%29+%282160p+BluRay+x265+HEVC+10bit+HDR+AAC+5.1+Tigole%29+%5BQxR%5D&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce`],
+                    id: "di-01",
+                    title: "S01E01",
+                    version: "1080p WEB h264-EDITH",
+                    size: "1.89 GB",
+                    seeders: 916,
+                    leechers: 1455,
+                    magnets: [String.raw`magnet:?xt=urn:btih:EDA093284DB6791008D6C24C6F8DA05D4F87D22E&dn=The+Dinosaurs+S01E01+1080p+WEB+h264-EDITH+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`],
                     torrents: null
                 },
                 {
-                    id: "sq-33",
-                    title: "Scream 3 (2000)",
-                    version: "(2160p BluRay x265 HEVC 10bit HDR AAC 5.1 Tigole) [QxR]",
-                    size: "11.2 GB",
-                    seeders: 37,
-                    leechers: 10,
-                    magnets: [String.raw`magnet:?xt=urn:btih:DDC97D0AF0B82457DC75864B39DEF808BDBE5F4C&dn=Scream+3+%282000%29+%282160p+BluRay+x265+HEVC+10bit+HDR+AAC+5.1+Tigole%29+%5BQxR%5D&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce`],
+                    id: "di-02",
+                    title: "S01E02",
+                    version: "2160p NF WEB-DL DDP5 1 Atmos DV HDR H 265-BiOMA",
+                    size: "6.39 GB",
+                    seeders: 138,
+                    leechers: 80,
+                    magnets: [String.raw`magnet:?xt=urn:btih:a3b0abb1ed77e89621ebd87c6913d684d04d06cf&dn=The+Dinosaurs+S01E02+2160p+NF+WEB-DL+DDP5+1+Atmos+DV+HDR+H+265-BiOMA+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`],
                     torrents: null
                 },
                 {
-                    id: "sq-34",
-                    title: "Scream 4 (2011)",
-                    version: "(1080p BluRay x265 10bit EAC3 5.1 Ghost) [QxR] ",
-                    size: "6.0 GB",
-                    seeders: 65,
-                    leechers: 19,
-                    magnets: [String.raw`magnet:?xt=urn:btih:DF52D3A23BCB5459179D3981E5179794F32D0574&dn=Scream+4+%282011%29+%281080p+BluRay+x265+10bit+EAC3+5.1+Ghost%29+%5BQxR%5D&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fwww.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Fopen.tracker.cl%3A1337%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce`],
+                    id: "di-03",
+                    title: "S01E03",
+                    version: "2160p NF WEB-DL DDP5 1 Atmos DV HDR H 265-BiOMA",
+                    size: "5.94 GB",
+                    seeders: 147,
+                    leechers: 77,
+                    magnets: [String.raw`magnet:?xt=urn:btih:f2a3a6424f004ebc22ff6fb94343ac20e1346bbb&dn=The+Dinosaurs+S01E03+2160p+NF+WEB-DL+DDP5+1+Atmos+DV+HDR+H+265-BiOMA+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`],
                     torrents: null
                 },
                 {
-                    id: "sq-35",
-                    title: "Scream (2022)",
-                    version: "(2160p BluRay x265 HEVC 10bit HDR AAC 7.1 Tigole) [QxR]",
-                    size: "12.2 GB",
-                    seeders: 38,
-                    leechers: 12,
-                    magnets: [String.raw`magnet:?xt=urn:btih:495316D6115480D94E33C582FB34C99CF94C7AE1&dn=Scream+%282022%29+%282160p+BluRay+x265+HEVC+10bit+HDR+AAC+7.1+Tigole%29+%5BQxR%5D&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce`],
-                    torrents: null
-                },
-                {
-                    id: "sq-36",
-                    title: "Scream VI (2023)",
-                    version: "(2160p BluRay x265 HEVC 10bit HDR AAC 7.1 Tigole) [QxR]",
-                    size: "11.9 GB",
-                    seeders: 25,
-                    leechers: 10,
-                    magnets: [String.raw`magnet:?xt=urn:btih:83611E3C67D7E2B4EB51ECDBF1E4339332D991F1&dn=Scream+VI+%282023%29+%282160p+BluRay+x265+HEVC+10bit+HDR+AAC+7.1+Tigole%29+%5BQxR%5D&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce`],
+                    id: "di-04",
+                    title: "S01E04",
+                    version: "2160p NF WEB-DL DDP5 1 Atmos DV HDR H 265-BiOMA",
+                    size: "5.96 GB",
+                    seeders: 164,
+                    leechers: 65,
+                    magnets: [String.raw`magnet:?xt=urn:btih:41412ae661de8ac5b2d8d3e97fdbab54e7a8333d&dn=The+Dinosaurs+S01E04+2160p+NF+WEB-DL+DDP5+1+Atmos+DV+HDR+H+265-BiOMA+%5B+UIndex.org+%5D%20%20%20%20%20%20%20%20&tr=udp://tracker.bittor.pw:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.opentrackr.org:1337/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.dler.org:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.stealth.si:80/announce%20%20%20%20%20%20%20%20&tr=udp://tracker.torrent.eu.org:451/announce%20%20%20%20%20%20%20%20&tr=udp://exodus.desync.com:6969/announce%20%20%20%20%20%20%20%20&tr=udp://open.demonii.com:1337/announce`],
                     torrents: null
                 }
             ]
         }
+    ]
     ]
 };
 
