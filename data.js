0a92278d804dbf119ebf844df89b5b401e788e24
leonardodastin <leonardodastin@gmail.com>
2026-06-22 00:22:09 +0530

    Anaconda Series

--- data.js
+++ data.js
@@ -37,6 +37,86 @@
 const DATA = {
     categories: [
         {
+            name: "Anaconda Series - 21GB",
+            items: [
+                {
+                    id: "ac-1",
+                    title: "Anaconda (1997)",
+                    version: "(1080p BluRay x265 10bit EAC3 5.1 Ghost) [QxR]",
+                    size: "6.8GB",
+                    seeders: 110,
+                    leechers: 79,
+                    magnets: [String.raw`magnet:?xt=urn:btih:5CD9B84F52BC13A06D8D88818DEAFE0FB2AC4E53&dn=Anaconda+%281997%29+%281080p+BluRay+x265+10bit+EAC3+5.1+Ghost%29+%5BQxR%5D&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fwww.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Fopen.tracker.cl%3A1337%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce`],
+                    torrents: null
+                },
+                {
+                    id: "ac-2",
+                    title: "Anacondas: The Hunt for the Blood Orchid (2004)",
+                    version: "(1080p BluRay x265 10bit EAC3 5.1 Celdra) [QxR]",
+                    size: "5.3GB",
+                    seeders: 83,
+                    leechers: 44,
+                    magnets: [String.raw`magnet:?xt=urn:btih:E33E9E8F5ADF1521455DFE0D4F4CBCDD5098B48E&dn=Anacondas%3A+The+Hunt+for+the+Blood+Orchid+%282004%29+%281080p+BluRay+x265+10bit+EAC3+5.1+Celdra%29+%5BQxR%5D&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.bittor.pw%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.free-tracker.ga%3A6969%2Fannounce&tr=udp%3A%2F%2Fns-1.x-fins.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fisk.richardsw.club%3A6969%2Fannounce&tr=udp%3A%2F%2Fhificode.in%3A6969%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=http%3A%2F%2Fwww.torrentsnipe.info%3A2701%2Fannounce&tr=http%3A%2F%2Fwww.genesis-sp.org%3A2710%2Fannounce&tr=http%3A%2F%2Ftracker.xiaoduola.xyz%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.vanitycore.co%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce`],
+                    torrents: null
+                },
+                {
+                    id: "ac-3",
+                    title: "Anaconda 3: Offspring (2008)",
+                    version: "(1080p BluRay x265 10bit EAC3 5.1 Celdra) [QxR]",
+                    size: "4.5GB",
+                    seeders: 84,
+                    leechers: 19,
+                    magnets: [String.raw`magnet:?xt=urn:btih:95E92907EDBFABFDFF3DF9AC73039B493888A959&dn=Anaconda+3%3A+Offspring+%282008%29+%281080p+BluRay+x265+10bit+EAC3+5.1+Celdra%29+%5BQxR%5D&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.bittor.pw%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.free-tracker.ga%3A6969%2Fannounce&tr=udp%3A%2F%2Fns-1.x-fins.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fisk.richardsw.club%3A6969%2Fannounce&tr=udp%3A%2F%2Fhificode.in%3A6969%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=http%3A%2F%2Fwww.torrentsnipe.info%3A2701%2Fannounce&tr=http%3A%2F%2Fwww.genesis-sp.org%3A2710%2Fannounce&tr=http%3A%2F%2Ftracker.xiaoduola.xyz%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.vanitycore.co%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce`],
+                    torrents: null
+                },
+                {
+                    id: "ac-4",
+                    title: "Anaconda 4: Trail of Blood (2009)",
+                    version: "Anacondas 4 Trail of Blood (2009) (1080p BluRay x265 10bit EAC3 5.1 Celdra) [QxR]",
+                    size: "4.4GB",
+                    seeders: 87,
+                    leechers: 28,
+                    magnets: [String.raw`magnet:?xt=urn:btih:68FD3BB299F690058CC37C49F5B667875E65C625&dn=Anacondas%3A+Trail+of+Blood+%282009%29+%281080p+BluRay+x265+10bit+EAC3+5.1+Celdra%29+%5BQxR%5D&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.bittor.pw%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.free-tracker.ga%3A6969%2Fannounce&tr=udp%3A%2F%2Fns-1.x-fins.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fisk.richardsw.club%3A6969%2Fannounce&tr=udp%3A%2F%2Fhificode.in%3A6969%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=http%3A%2F%2Fwww.torrentsnipe.info%3A2701%2Fannounce&tr=http%3A%2F%2Fwww.genesis-sp.org%3A2710%2Fannounce&tr=http%3A%2F%2Ftracker.xiaoduola.xyz%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.vanitycore.co%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce`],
+                    torrents: null
+                }
+            ]
+        },
+        {
+            name: "Movies",
+            items: [
+                {
+                    id: "mv-1",
+                    title: "Pressure (2026)",
+                    version: "2160p.iT.WEB-DL.DV.HDR10+.DDP5.1.Atmos.H265.MP4-BTM ",
+                    size: "18.31GB",
+                    seeders: 335,
+                    leechers: 161,
+                    magnets: [String.raw`magnet:?xt=urn:btih:c92355ca5f9e350678396e4e9281be5aa3f5038b&dn=Pressure.2026.2160p.iT.WEB-DL.DV.HDR10 .DDP5.1.Atmos.H265.MP4-BEN.THE.MEN&xl=19660954718&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://open.stealth.si:80/announce&tr=udp://tracker.torrent.eu.org:451/announce&tr=udp://tracker.opentrackr.org:1337`],
+                    torrents: null
+                },
+                {
+                    id: "mv-2",
+                    title: "Deep Water 2026",
+                    version: "1080p AMZN WEB-DL DDP5 1 H 264-KyoGo ",
+                    size: "7.16GB",
+                    seeders: 276,
+                    leechers: 170,
+                    magnets: [String.raw`magnet:?xt=urn:btih:D074961F4C810ADCBE59BCD325B2A6E39F93B36F&dn=Deep Water 2026 2026 1080p AMZN WEB-DL DDP5 1 H 264-KyoGo&tr=udp://tracker.opentrackr.org:1337&tr=udp://open.stealth.si:80/announce&tr=udp://tracker.torrent.eu.org:451/announce&tr=udp://tracker.bittor.pw:1337/announce&tr=udp://public.popcorn-tracker.org:6969/announce&tr=udp://tracker.dler.org:6969/announce&tr=udp://exodus.desync.com:6969&tr=udp://open.demonii.com:1337/announce&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://torrent.gresille.org:80/announce&tr=udp://p4p.arenabg.com:1337&tr=udp://tracker.internetwarriors.net:1337`],
+                    torrents: null
+                },
+                {
+                    id: "mv-3",
+                    title: "They Will Kill You 2026",
+                    version: "2160p.iT.WEB-DL.DDP5.1.Atmos.DV.HDR.H.265-SCOPE ",
+                    size: "16.68GB",
+                    seeders: 196,
+                    leechers: 91,
+                    magnets: [String.raw`magnet:?xt=urn:btih:d87ee26d833d6a57e622480470e4d0ee88d1a643&dn=They.Will.Kill.You.2026.2160p.iT.WEB-DL.DDP5.1.Atmos.DV.HDR.H.265-SCOPE&xl=17911348904&tr=udp://tracker.opentrackr.org:1337/announce&tr=http://tracker.opentrackr.org:1337/announce&tr=http://lucke.fenesisu.moe:6969/announce&tr=http://bt1.us.archive.org:6969/announce&tr=http://bt2.us.archive.org:6969/announce&tr=http://tracker.dhitechnical.com:6969/announce&tr=http://tracker.darkness.services:6969/announce&tr=http://bt.okmp3.ru:2710/announce&tr=http://open.acgtracker.com:1096/announce&tr=http://pow7.com/announce&tr=http://tracker.beeimg.com:6969/announce&tr=http://tracker.bt4g.com:2095/announce&tr=http://tracker.internetwarriors.net:1337/announce&tr=udp://tracker.alaskantf.com:6969/announce&tr=https://tracker.alaskantf.com:443/announce&tr=udp://6ahddutb1ucc3cp.ru:6969/announce&tr=udp://exodus.desync.com:6969&tr=udp://explodie.org:6969/announce&tr=udp://glotorrents.pw:6969/announce&tr=udp://martin-gebhardt.eu:25/announce&tr=udp://ns-1.x-fins.com:6969/announce&tr=udp://open.demonii.com:1337&tr=udp://open.free-tracker.ga:6969/announce&tr=udp://open.stealth.si:80/announce&tr=udp://open.tracker.cl:1337/announce&tr=udp://opentracker.io:6969/announce&tr=udp://p4p.arenabg.com:1337/announce&tr=udp://retracker01-msk-virt.corbina.net:80/announce&tr=udp://run.publictracker.xyz:6969/announce&tr=udp://tracker-udp.gbitt.info:80/announce&tr=udp://tracker.0x7c0.com:6969/announce&tr=udp://tracker.bittor.pw:1337/announce&tr=udp://tracker.dler.org:6969/announce&tr=udp://tracker.fnix.net:6969/announce&tr=udp://tracker.qu.ax:6969/announce&tr=udp://tracker.srv00.com:6969/announce&tr=udp://tracker.theoks.net:6969/announce&tr=udp://tracker.torrent.eu.org:451/announce&tr=udp://tracker.uw0.xyz:6969/announce&tr=udp://tracker.vanitycore.co:6969/announce&tr=udp://zer0day.ch:1337&tr=https://tracker.wildkat.net:8443/announce&tr=udp://tracker.wildkat.net:6969/announce`],
+                    torrents: null
+                }
+            ]
+        }
+        /*,{
             name: "Breaking Bad",
             items: [
                 {
@@ -62,7 +142,7 @@ const DATA = {
                     torrents: null
                 }
             ]
-        }
+        }*/
 
     ]
 };
